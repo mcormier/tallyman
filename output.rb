c2977c7a636f62fb83c3eb4ed24b9e7429963649
Matthieu <mcormier@mail.preenandprune.com>
2012-12-10 18:49:49 +0000

    Output lift data to XML

--- output.rb
+++ output.rb
@@ -6,6 +6,19 @@ require "sqlite3"
 
 load 'config.properties'
 
+def queryLiftData( db, query, liftname, rep)
+  retVal = "0"
+
+  value = db.get_first_value query, liftname, rep 
+  if not value.nil? then
+    retVal = value
+  end
+
+  return retVal
+end
+
+
+
 
 begin
   x = Builder::XmlMarkup.new( :indent => 2 )
@@ -14,18 +27,41 @@ begin
 
   File.open( @outputFile, 'w' ) do |out|
 
-  out.puts x.data {
+  #out.puts x.data {
+  puts x.data {
     @outputQueries.each do |queryInfo|
-      stm = db.prepare queryInfo[0]
-      rs = stm.execute
-      row = rs.next
-      x.item{
-        x.title queryInfo[1]
-        x.value row.join "\s"
-      }
-      stm.close
+      begin
+        stm = db.prepare queryInfo[0]
+        rs = stm.execute
+        row = rs.next
+        x.item{
+          x.title queryInfo[1]
+          x.value row.join "\s"
+        }
+      ensure
+        stm.close
+      end
     end
+
+    x.lifts {
+    @lifts.each do |lift|
+      begin
+        
+        x.lift {
+          x.name lift
+
+          x.onerm queryLiftData( db, @liftQuery, lift, 1)
+          x.threerm queryLiftData( db, @liftQuery, lift, 3)  
+          x.fiverm  queryLiftData( db, @liftQuery, lift, 5)
+        }
+      ensure
+        #stm.close
+      end
+    end
+    }
   }
+
+ 
 
   end
 
