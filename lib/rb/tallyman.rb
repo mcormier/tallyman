7092e3585742cc8743de06deed1b1f58278695b4
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-08 11:55:18 -0300

    group action files

--- lib/rb/tallyman.rb
+++ lib/rb/tallyman.rb
@@ -2,6 +2,6 @@
 
 require_relative 'action/book_action'
 require_relative 'action/music_action'
-require_relative 'lift_action'
-require_relative 'count_action'
+require_relative 'action/lift_action'
+require_relative 'action/count_action'
 require_relative 'database_proxy'
