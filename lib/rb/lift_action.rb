690191e825e621c57d32acc9b75d4b6695b54640
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-08 11:24:37 -0300

    Ruby style method declarations

--- lib/rb/lift_action.rb
+++ lib/rb/lift_action.rb
@@ -1,43 +1,43 @@
-require "ppcurses"
+require 'ppcurses'
 
 class LiftAction < PPCurses::InsertSQLDataAction
 
-  def initialize(nameMenu, repMenu, db)
-    @nameMenu = nameMenu
-    @repMenu = repMenu
+  def initialize(name_menu, rep_menu, db)
+    @name_menu = name_menu
+    @rep_menu = rep_menu
     @db = db
     
-    @prompt  = PPCurses::GetIntegerAction.new("Weight (pounds) : ")
+    @prompt  = PPCurses::GetIntegerAction.new('Weight (pounds) : ')
     super( [ @prompt ], 'INSERT into LIFTS(name, weight, reps) values (?, ?, ?)', @db )
 
   end
 
-  def winHeight()
+  def winHeight
     9
   end
  
-  def liftName()
-    @nameMenu.getSelectedMenuName()
+  def lift_name
+    @name_menu.getSelectedMenuName()
   end
   
-  def repsName()
-    @repMenu.getSelectedMenuName()
+  def reps_name
+    @rep_menu.getSelectedMenuName()
   end
   
-  def repsInteger()
-    Integer(repsName().chars.first).to_s
+  def reps_integer
+    Integer(reps_name().chars.first).to_s
   end
 
-  def beforeActions()
-    self.printLine('Input data for ' + repsName() + ' ' + liftName() )
+  def before_actions
+    self.printLine('Input data for ' + reps_name() + ' ' + lift_name() )
   end
 
-  def afterActions()
-    preparedSql = @sql.sub('%s', liftName() )
-    preparedSql = preparedSql.sub('%s', @prompt.data() )
-    preparedSql = preparedSql.sub('%s', repsInteger() )
+  def after_actions
+    prepared_sql = @sql.sub('%s', lift_name() )
+    prepared_sql = prepared_sql.sub('%s', @prompt.data() )
+    prepared_sql = prepared_sql.sub('%s', reps_integer() )
 
-    self.promptToChangeData(preparedSql, [liftName(), @prompt.data(), repsInteger ])
+    self.prompt_to_change_data(prepared_sql, [lift_name(), @prompt.data(), reps_integer() ])
   end
 
 end
