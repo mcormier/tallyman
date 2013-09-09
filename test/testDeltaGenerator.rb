13c7b3ff64e456bb1a14f2fc8b49c8cc477d302c
Matthieu Cormier <mcormier@cs.dal.ca>
2013-09-08 22:00:59 -0300

    test empty set

--- test/testDeltaGenerator.rb
+++ test/testDeltaGenerator.rb
@@ -6,6 +6,10 @@ require_relative '../lib/rb/tallyman'
 @modified_table_set = Set.new()
 @modified_table_set.add('lifts')
 
+# Test an empty set
+generator = DeltaGenerator.new(Set.new())
+generator.generate
+
 generator = DeltaGenerator.new(@modified_table_set)
 generator.generate
-generator.generate('delta.xml')
+#generator.generate('delta.xml')
