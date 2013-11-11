330aaee9032fbda8acbcef18b072feb759b17311
Matthieu Cormier <mcormier@cs.dal.ca>
2013-11-11 12:45:05 -0400

    svg file creation moved to _transform of theme.

--- data/themes/feltron2006/_transform/createGraphs.rb
+++ data/themes/feltron2006/_transform/createGraphs.rb
@@ -1,1 +1,12 @@
 puts "TODO create graphs"
+
+transform_root = @theme_root +'/_transform/'
+graphing_root = transform_root + 'graphing/'
+
+puts graphing_root
+
+
+cmd = graphing_root + 'createGraphs.sh ' + @dbName + ' ' + @webRoot
+value = `#{cmd}`
+
+puts value
