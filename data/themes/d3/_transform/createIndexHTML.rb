ff0f4eaa679438d30b13bdadcc536d0acb9739d5
Matthieu Cormier <mcormier@cs.dal.ca>
2013-11-11 12:11:02 -0400

    move xsl into the _transform directory

--- data/themes/d3/_transform/createIndexHTML.rb
+++ data/themes/d3/_transform/createIndexHTML.rb
@@ -1,5 +1,5 @@
 
-xsl_loc= @theme_root +'/xsl/createHTML.xsl'
+xsl_loc= @theme_root +'/_transform/xsl/createHTML.xsl'
 
 cmd = 'xsltproc ' + xsl_loc + ' ' + @data_file + ' > ' + @outputHTMLFile
 value = `#{cmd}`
