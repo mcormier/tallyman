f5065258466bd1a2e7cab1cef4e40cab0ee8a9b2
Matthieu Cormier <mcormier@cs.dal.ca>
2013-11-11 12:16:47 -0400

    generate tsv file

--- data/themes/d3/_extract/tsv.rb
+++ data/themes/d3/_extract/tsv.rb
@@ -1,1 +1,5 @@
-puts "TODO - generate tsv files"
+sql_cmds = @theme_root + "/_extract/lifts.sqlite"
+
+out_file = @webRoot + 'lifts.tsv'
+
+`sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
--- data/themes/d3/dbScript.rb
+++ data/themes/d3/dbScript.rb
@@ -3,7 +3,7 @@
 #puts "database: " + @dbName
 
 # TODO - make generic
-sqlCmds = "/Users/mcormier/Portfolio/GIT/tallyman/data/themes/d3/extractor"
+sqlCmds = "/Users/mcormier/Portfolio/GIT/tallyman/data/themes/d3/lifts.sqlite"
 
 outFile = @webRoot + 'lifts.tsv'
 
