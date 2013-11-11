318ff4e4762b3c9d0a01cb577dc619ecf0a727ca
Matthieu Cormier <mcormier@cs.dal.ca>
2013-11-11 13:03:05 -0400

    update cache manifest

--- data/themes/feltron2006/_transform/updateCacheManifest.rb
+++ data/themes/feltron2006/_transform/updateCacheManifest.rb
@@ -1,1 +1,7 @@
-puts "TODO -- update cache manifest"
+cache_manifest = @theme_root +'/cache/cache.manifest'
+
+now = `date +"%s"`.strip!
+cmd =  "sed '2 s/.*/#\ Version:#{now}/' #{cache_manifest} > #{@webRoot}/cache.manifest"
+
+`#{cmd}`
+
