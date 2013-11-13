e9fff04b496aba99fa8d392641bd37523acffbc2
Matt Cormier <mcormier@preenandprune.com>
2013-11-13 00:22:56 +0000

    add books tsv file

--- data/themes/d3/_extract/tsv.rb
+++ data/themes/d3/_extract/tsv.rb
@@ -3,3 +3,9 @@ sql_cmds = @theme_root + "/_extract/lifts.sqlite"
 out_file = @webRoot + 'lifts.tsv'
 
 `sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
+
+out_file = @webRoot + 'books.tsv'
+
+sql_cmds = @theme_root + "/_extract/books.sqlite"
+
+`sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
