721983491daa8929b2e543a4291079bed857a3dc
Matthieu Cormier <mcormier@cs.dal.ca>
2013-11-16 08:17:48 -0400

    generify

--- data/themes/d3/_extract/tsv.rb
+++ data/themes/d3/_extract/tsv.rb
@@ -1,11 +1,5 @@
-sql_cmds = @theme_root + "/_extract/lifts.sqlite"
-out_file = @webRoot + 'lifts.tsv'
-`sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
-
-out_file = @webRoot + 'books.tsv'
-sql_cmds = @theme_root + "/_extract/books.sqlite"
-`sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
-
-out_file = @webRoot + 'music.tsv'
-sql_cmds = @theme_root + "/_extract/music.sqlite"
-`sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
+%w(lifts books music).each do |tsvType|
+  out_file = "#{@webRoot}#{tsvType}.tsv"
+  sql_cmd_file = "#{@theme_root}/_extract/#{tsvType}.sqlite"
+  `sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
+end
