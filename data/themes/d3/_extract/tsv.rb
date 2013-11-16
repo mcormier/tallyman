de688216a178a559335904d713ee0f3e4cac7889
Matthieu Cormier <mcormier@cs.dal.ca>
2013-11-16 09:21:17 -0400

    bug fix.

--- data/themes/d3/_extract/tsv.rb
+++ data/themes/d3/_extract/tsv.rb
@@ -1,5 +1,5 @@
 %w(lifts books music).each do |tsvType|
   out_file = "#{@webRoot}#{tsvType}.tsv"
   sql_cmd_file = "#{@theme_root}/_extract/#{tsvType}.sqlite"
-  `sqlite3 #{@dbName} < #{sql_cmds} > #{out_file} `
+  `sqlite3 #{@dbName} < #{sql_cmd_file} > #{out_file} `
 end
