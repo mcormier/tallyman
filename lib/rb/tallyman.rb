577b3f9594568bf388f365c0391276ae4b3aeeac
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-19 15:35:29 -0300

    Add reading_domain

--- lib/rb/tallyman.rb
+++ lib/rb/tallyman.rb
@@ -13,6 +13,7 @@ require_relative 'subscriber/delta_notifier'
 
 require_relative 'domain/domain_manager'
 require_relative 'domain/domain'
+require_relative 'domain/reading_domain'
 
 module Tallyman
   require_relative 'config/config_loader'
