69bc307a8d5464379e3cbdf2c7817716e9c4f3e2
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-20 17:19:39 -0300

    Make table name based on config.yaml

--- lib/rb/domain/cycling_domain.rb
+++ lib/rb/domain/cycling_domain.rb
@@ -3,24 +3,22 @@ require 'ppcurses'
 class CyclingDomain < Domain
 
   def create_action( db )
-    CycleAction.new(db)
+    CycleAction.new(db, table_name)
   end
 
 end
 
 
 
-
-
 class CycleAction < PPCurses::InsertSQLDataAction
 
-  def initialize(db)
+  def initialize(db, table_name)
     @db = db
+    @table_name = table_name
 
-    # TODO - table name shouldn't be hard coded.
     super( [PPCurses::GetIntegerAction.new('Distance: '),
             PPCurses::GetIntegerAction.new('Duration: '), ],
-           'INSERT into cycleLog(distance_km, duration ) values (?, ?)', db )
+           "INSERT into #{@table_name}(distance_km, duration ) values (?, ?)", db )
   end
 
 
@@ -28,7 +26,7 @@ class CycleAction < PPCurses::InsertSQLDataAction
     new_data_added = super
 
     if new_data_added then
-      @db.data_added_to('cycleLog')
+      @db.data_added_to( @table_name )
     end
 
   end
