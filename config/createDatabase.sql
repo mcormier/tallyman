836c9a3e1f35d5c04ba7e1c413c5cf5934f46868
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-19 21:30:57 -0300

    Push SQL to domain.

--- config/createDatabase.sql
+++ config/createDatabase.sql
@@ -7,17 +7,6 @@
 -- 
 create table countTable(event varchar(256), day date default current_date ); 
 
--- Music
--- =====
--- Media (CD, Record, MP3)
--- Used 0 = False, 1 = True
---
-create table music(media varchar(256), 
-                   artist varchar(256), 
-                   albumTitle varchar(256), 
-                   price smallint, 
-                   used boolean default 0, 
-                   dayPurchased date default current_date );
 
 -- Same as CountTable but for counting events with a value associated with them
 -- i.e. 10 pushups ...
