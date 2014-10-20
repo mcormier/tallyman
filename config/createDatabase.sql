52c0d092bb39c58a5a58e3331f124bd01826c1a9
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-20 17:07:18 -0300

    Database is created when needed.  Tables are created the first time they are needed by the domain.

--- config/createDatabase.sql
+++
@@ -1,9 +0,0 @@
-
-
-
--- Same as CountTable but for counting events with a value associated with them
--- i.e. 10 pushups ...
-create table valueTable(event varchar(256),value smallint, day date default current_date );
-
-.quit
-
