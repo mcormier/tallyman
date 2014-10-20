5c9d2304216eefd9d2b8d9dba95bdaeb69cca1ba
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-20 17:06:18 -0300

    Remove sql statements.

--- config/createDatabase.sql
+++ config/createDatabase.sql
@@ -1,12 +1,5 @@
 
 
--- CountTable
--- ==========
--- A table for counting events that occur infrequently, and will occur once a day at most.
--- i.e. Stubbed Toe - March 12th
--- 
-create table countTable(event varchar(256), day date default current_date ); 
-
 
 -- Same as CountTable but for counting events with a value associated with them
 -- i.e. 10 pushups ...
