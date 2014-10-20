0af64b81364b50b97e3f39c9ea15641191c473c4
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-20 17:15:35 -0300

    Implement cycling_domain

--- lib/rb/domain/cycling_domain.rb
+++ lib/rb/domain/cycling_domain.rb
@@ -1,3 +1,36 @@
+require 'ppcurses'
+
 class CyclingDomain < Domain
+
+  def create_action( db )
+    CycleAction.new(db)
+  end
+
+end
+
+
+
+
+
+class CycleAction < PPCurses::InsertSQLDataAction
+
+  def initialize(db)
+    @db = db
+
+    # TODO - table name shouldn't be hard coded.
+    super( [PPCurses::GetIntegerAction.new('Distance: '),
+            PPCurses::GetIntegerAction.new('Duration: '), ],
+           'INSERT into cycleLog(distance_km, duration ) values (?, ?)', db )
+  end
+
+
+  def after_actions
+    new_data_added = super
+
+    if new_data_added then
+      @db.data_added_to('cycleLog')
+    end
+
+  end
 
 end
