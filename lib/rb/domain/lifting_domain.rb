cba862e2c38399d3d8d0049816095752fefeca86
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-22 22:09:26 -0300

    First config impl

--- lib/rb/domain/lifting_domain.rb
+++ lib/rb/domain/lifting_domain.rb
@@ -1,11 +1,13 @@
 class LiftingDomain < Domain
 
+  @@lift_list =  ['Deadlift', 'Shoulder Press', 'Clean', 'Front Squat', 'Push Jerk', 'Overhead Squat', 'Snatch']
+
   def create_action( db )
 
     # TODO -- make configurable
     # TODO -- Push 1/3/5 RM selection inside menu
     lift_rep_menu = PPCurses::RadioMenu.new( %w(1RM 3RM 5RM), nil )
-    lift_type_menu = PPCurses::Menu.new( ['Deadlift', 'Shoulder Press', 'Clean', 'Front Squat', 'Push Jerk', 'Overhead Squat', 'Snatch'] , nil )
+    lift_type_menu = PPCurses::Menu.new( @@lift_list , nil )
     lifts_menu = PPCurses::CompositeMenu.new( lift_type_menu, lift_rep_menu )
     add_lift_action = LiftAction.new( lift_type_menu, lift_rep_menu, db )
 
@@ -15,5 +17,22 @@ class LiftingDomain < Domain
   end
 
 
+  # TODO -- display a selectable list of lifts.
+  def config_action( config )
+
+    lift_menu_items = []
+
+    @@lift_list.each do |lift_name|
+      item = PPCurses::MenuItem.new(lift_name )
+      item.selectable=true
+      # TODO -- get item state from the config object.
+      lift_menu_items.push(item)
+    end
+
+    config_menu = PPCurses::Menu.new( lift_menu_items, nil )
+    #config_menu.show
+    #config_menu.menu_selection
+    PPCurses::ShowMenuAction.new(config_menu)
+  end
 
 end
