844be03c5ca594d5150e2df69d645a86081a0585
Matthieu Cormier <mcormier@cs.dal.ca>
2014-10-23 21:07:44 -0300

    Add active lifts to configuration.

--- lib/rb/domain/lifting_domain.rb
+++ lib/rb/domain/lifting_domain.rb
@@ -17,20 +17,69 @@ class LiftingDomain < Domain
   end
 
 
-  # TODO -- display a selectable list of lifts.
+  def receive_message(menu_item)
+
+    title = menu_item.title
+
+    if menu_item.state == PPCurses::PP_ON_STATE
+      @lifting_config.enabled_lifts.add(title)
+    else
+      @lifting_config.enabled_lifts.delete(title)
+    end
+  end
+
+
+
+  # Displays a selectable list of lifts.
   def config_action( config )
+
+    @lifting_config = config.get_domain_config('lifting')
+
+    if @lifting_config.nil?
+      @lifting_config = LiftingConfig.new
+    end
 
     lift_menu_items = []
 
     @@lift_list.each do |lift_name|
       item = PPCurses::MenuItem.new(lift_name )
       item.selectable=true
-      # TODO -- get item state from the config object.
+
+      if @lifting_config.lift_enabled?(lift_name)
+        item.state=PPCurses::PP_ON_STATE
+      end
+
+      item.target=method(:receive_message)
+
       lift_menu_items.push(item)
     end
+
+
+    config.set_domain_config( @lifting_config, 'lifting')
 
     config_menu = PPCurses::Menu.new( lift_menu_items, nil )
     PPCurses::ShowMenuAction.new(config_menu)
   end
+
+end
+
+
+class LiftingConfig
+
+  attr_accessor :enabled_lifts
+
+  def initialize
+    @enabled_lifts = Set.new
+  end
+
+  def lift_enabled?(lift_name)
+    if enabled_lifts.nil?
+      return false
+    end
+
+    enabled_lifts.member?(lift_name)
+
+  end
+
 
 end
