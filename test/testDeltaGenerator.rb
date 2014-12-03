7294d707c3bb2faac185626120892a808449df2f
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-03 19:59:39 -0400

    Style.

--- test/testDeltaGenerator.rb
+++ test/testDeltaGenerator.rb
@@ -3,11 +3,11 @@
 require 'set'
 require_relative '../lib/rb/tallyman'
 
-@modified_table_set = Set.new()
+@modified_table_set = Set.new
 @modified_table_set.add('lifts')
 
 # Test an empty set
-generator = DeltaGenerator.new(Set.new())
+generator = DeltaGenerator.new(Set.new)
 generator.generate
 
 generator = DeltaGenerator.new(@modified_table_set)
