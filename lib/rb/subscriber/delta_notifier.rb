94969a9225dbbea65b70b453b786cebaacc164bc
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-03 19:58:25 -0400

    nokogiri loaded at library level.

--- lib/rb/subscriber/delta_notifier.rb
+++ lib/rb/subscriber/delta_notifier.rb
@@ -1,15 +1,13 @@
-require 'nokogiri'
-
 class DeltaNotifier
 
   def initialize(filename)
     xml = File.open( filename)
     doc = Nokogiri::XML(xml)
 
-    @lifts  = doc.xpath('//table[text()="lifts"]').length() != 0
-    @books  = doc.xpath('//table[text()="books"]').length() != 0
-    @music  = doc.xpath('//table[text()="music"]').length() != 0
-    @misc  = doc.xpath('//table[text()="music"]').length() != 0
+    @lifts  = doc.xpath('//table[text()="lifts"]').length != 0
+    @books  = doc.xpath('//table[text()="books"]').length != 0
+    @music  = doc.xpath('//table[text()="music"]').length != 0
+    @misc  = doc.xpath('//table[text()="music"]').length != 0
 
   end
 
