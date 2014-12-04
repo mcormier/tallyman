0e30aa656a9589cb0cbb129c8ec1180a8d7e411c
Matthieu Cormier <mcormier@cs.dal.ca>
2014-12-03 21:54:09 -0400

    Use database proxy.

--- test/testCountAction.rb
+++ test/testCountAction.rb
@@ -7,17 +7,18 @@ require_relative '../lib/rb/tallyman'
 
 load '../config/config.properties'
 
-def getData(db)
-  countAction = CountAction.new(db)
-  countAction.show()
-  countAction.menu_selection()
+def get_data(db)
+  count_action = CountAction.new(db)
+  count_action.show
+  count_action.menu_selection
 end
 
 begin
-  db = SQLite3::Database.open @dbName
 
-  screen = PPCurses::Screen.new()
-  screen.run { getData(db) }
+  db = DatabaseProxy.open @dbName
+
+  screen = PPCurses::Screen.new
+  screen.run { get_data(db) }
 
 rescue SystemExit, Interrupt
   # Empty Catch block so ruby doesn't puke out
