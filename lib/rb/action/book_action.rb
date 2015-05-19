f8a75d42c0e2d83ecbdee8b3c7d7bdbe1d24a67c
Matthieu Cormier <mcormier@cs.dal.ca>
2015-05-19 09:15:40 -0300

    Remove old style action.

--- lib/rb/action/book_action.rb
+++ lib/rb/action/book_action.rb
@@ -55,28 +55,3 @@ class ReadingAction
  
 
 end
-
-
-# @deprecated.  Remove in Tallyman 1.4.
-class BookAction < PPCurses::InsertSQLDataAction
-
-  def initialize(db)
-    @db = db
-
-    super( [PPCurses::GetStringAction.new('Book title: '),
-            PPCurses::GetStringAction.new('Author: '),
-            PPCurses::GetIntegerAction.new('Number of pages: '),
-            PPCurses::GetBooleanAction.new('Digital? '), ],
-          'INSERT into books(title, author, pages, digital) values (?, ?, ?, ?)', db )
-  end
-
-  def after_actions
-    new_data_added = super
-
-    if new_data_added then
-      @db.data_added_to('books')
-    end
-
-  end
-
-end
