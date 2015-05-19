0b835ac67355d929549d9a082f3e2e26a49d7d2d
Matthieu Cormier <mcormier@cs.dal.ca>
2015-05-19 16:16:36 -0300

    old code replaced by gas_action

--- test/testFuelAction.rb
+++ test/testFuelAction.rb
@@ -1,14 +1,11 @@
 #!/usr/bin/env ruby
 
-gem 'ppcurses', '=0.1.0'
-require 'ppcurses'
-
 require_relative '../lib/rb/tallyman'
 
 
 @app = PPCurses::Application.new
 
-fuel_action = FuelAction.new
+fuel_action = GasAction.new
 
 @app.content_view = fuel_action.form
 
