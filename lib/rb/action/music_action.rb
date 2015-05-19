740be1decce6ea427a453ba1cdd47a3cd9dff506
Matthieu Cormier <mcormier@cs.dal.ca>
2015-05-19 09:18:12 -0300

    Remove old style action.

--- lib/rb/action/music_action.rb
+++ lib/rb/action/music_action.rb
@@ -66,33 +66,3 @@ class MusicAction_10
 
 
 end
-
-# @deprecated.  Remove in Tallyman 1.4.
-class MusicAction < PPCurses::InsertSQLDataAction
-
-  def initialize(db)
-    @db = db
-
-    super( [PPCurses::GetEnumeratedStringAction.new('Media Type? ',
-                                                    %w(CD Vinyl MP3)),
-            PPCurses::GetStringAction.new('Artist: '),
-            PPCurses::GetStringAction.new('Album Title: '),
-            PPCurses::GetIntegerAction.new('Price: '),
-            PPCurses::GetBooleanAction.new('Used? '), ],
-           'INSERT into music(media, artist, albumTitle, price, used) values (?, ?, ?, ?, ?)', db )
-
-  end
-
-
-
-  # TODO duplicate logic
-  def after_actions
-    new_data_added = super
-
-    if new_data_added then
-      @db.data_added_to('music')
-    end
-
-  end
-
-end
