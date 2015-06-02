f05b059fc4b7e9c320a13deded3660245ff1bec4
Matthieu Cormier <mcormier@cs.dal.ca>
2015-06-01 23:41:50 -0300

    Snapshot for 1.4

---
+++ demo/1.4/js/setupGraphs.js
@@ -0,0 +1,3 @@
+var gDim = { 'w': 830, 'h': 300, 'topMargin': 20, 'bottomMargin':80, 'leftMargin': 80, 'rightMargin': 80 };
+var liftNames = [ 'Deadlift','Shoulder Press','Clean','Front Squat','Push Jerk','Overhead Squat','Snatch','Back Squat'];
+var orchid = new PPRepGraphOrchestrator(liftNames, 'lifts.tsv',  gDim);
